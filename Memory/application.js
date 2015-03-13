9938be2d4090ed829edd49634062f6b7ec30147a
rocky26 <rvillanueva@kippnyccp.org>
2015-03-13 13:43:00 -0400

    Update application.js

--- Memory/application.js
+++ Memory/application.js
@@ -1,5 +1,50 @@
 $(document).ready(function(){
-  for(i=1; i<21; i++){
-    $('#card_holder').append('<div class="card"><p>'+i+'</p></div>');
+  var card_array = ["fish", "fish", "turtle", "turtle", "pig", "pig", "dog", "dog", "cat", "cat", "snake", "snake", "bird", "bird", "goat", "goat", "hamster", "hamster", "dragon", "dragon"]
+  
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex ;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
   }
+
+  return array;
+}
+  var shuffled_array = shuffle(card_array)
+  for(i in card_array){
+    $('#card_holder').append('<div class="card"><p>'+card_array[i]+'</p></div>');
+  }
+  var card_1=0;
+  var card_2=2;
+  var click_count = 'first';
+  $(".card").click(function(){
+    if(click_count == 'first'){
+    	$(this).find("p").css("opacity", 1).addClass('clicked');
+    	card_1 = $(this).find("p").html();
+      click_count="second";
+    }
+    else{
+      $(this).find("p").css("opacity", 1).addClass("clicked");
+    	card_2 = $(this).find("p").html();
+      click_count="first";
+      if(card_1==card_2){
+        $('.clicked').css("color","blue").removeClass('clicked');
+      }
+      else{
+        $('.clicked').css("opacity",0).removeClass('clicked');
+      }
+    }
+  })
+  $("#clear").click(function(){
+    $(document).find("p").css("opacity", 0)
+  })
 });
