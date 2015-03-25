d3128eb157bd47968c8ef355f07914501018aa21
rocky26 <rvillanueva@kippnyccp.org>
2015-03-25 13:14:28 -0400

    Update application.js

--- shoppinglist/application.js
+++ shoppinglist/application.js
@@ -2,6 +2,8 @@ $(document).ready(function(){
  $("#pie").click(function(){
     var item = $('#input').val();
     $('#boxes').append("<li class='box'>"+item+"</li>");
+  
+ });
  $("#empty").click(function(){
     var item = $('#input').val();
     $('#boxes').empty();
