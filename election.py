585f39e28c2fc563c5e00aad276d4aec31587dda
rocky26 <rvillanueva@kippnyccp.org>
2015-05-11 13:39:55 -0400

    Update election.py

--- election.py
+++ election.py
@@ -1,16 +1,17 @@
 from random import randint
-
+total=0
 for i in range(10000):
-  rolls = randint(1,100)
   wins=0 
-  if rolls <= 87:
+  if randint(1,100) <= 87:
     wins=wins+1
-  if rolls <= 65:
+  if randint(1,100) <= 65:
     wins=wins+1
-  if rolls <= 17: 
+  if randint(1,100) <= 17: 
     wins=wins+1
-  if wins >=2: 
-    print "you win the election." 
-  if wins<=2"
-  print "you lost the election."
+  if wins>=2
+    total+=1 
+winning= (total/((float(10000)))
+print "your chance in winning is %s" & chance
+  
+  
   
