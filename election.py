08d14df581fe6cf52063e881bc6fe3d23b89a704
rocky26 <rvillanueva@kippnyccp.org>
2015-05-11 13:09:22 -0400

    Update election.py

--- election.py
+++ election.py
@@ -1,1 +1,16 @@
+from random import randint
 
+for i in range(10000):
+  rolls = randint(1,100)
+  wins=0 
+  if rolls <= 87:
+    wins=wins+1
+  if rolls <= 65:
+    wins=wins+1
+  if rolls <= 17: 
+    wins=wins+1
+  if wins >=2: 
+    print "you win the election." 
+  if wins<=2"
+  print "you lost the election."
+  
