ae54ad31887d6bdc9182b9938a4ecb6e76628e90
rocky26 <rvillanueva@kippnyccp.org>
2015-05-12 13:15:47 -0400

    Update election.py

--- election.py
+++ election.py
@@ -1,17 +1,17 @@
 from random import randint
-total=0
-for i in range(10000):
-  wins=0 
-  if randint(1,100) <= 87:
-    wins=wins+1
-  if randint(1,100) <= 65:
-    wins=wins+1
-  if randint(1,100) <= 17: 
-    wins=wins+1
-  if wins>=2
-    total+=1 
-winning= (total/((float(10000)))
-print "your chance in winning is %s" % chance
+total=0 # sets the total elections won 
+for i in range(10000): # repeats procces 10000 times because the range is 10,000
+  wins=0  #sets wins in regions to 0 
+  if randint(1,100) <= 87: #picks a number between 1-100 and if its between 1-87 - follow next step 
+    wins=wins+1 # add smaller regional wins to wins 
+  if randint(1,100) <= 65:  #picks a number between 1-100 and if its between 1-64 - follow next step 
+    wins=wins+1# add smaller regional wins to wins 
+  if randint(1,100) <= 17: #picks a number between 1-100 and if its between 1-17 - follow next step 
+    wins=wins+1 # add smaller regional wins to wins 
+  if wins>=2: #dettermined if has 2 or more regional wins
+    total+=1 # if so thean adds to toal aka the complete election 
+chance= (total/((float(10000))) # makes it into a percent to print out 
+print "your chance in winning is %s" % chance #prints out statement and percent 
   
   
   
